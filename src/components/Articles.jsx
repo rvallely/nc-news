15be2ffe31e48729db9457bfc03a210dff08d00e
rvallely <68432429+rvallely@users.noreply.github.com>
2022-02-08 13:43:08 +0000

    map all elements articles

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { getArticles } from '../utils/api';
 import Nav from './Nav';
 import { useParams } from 'react-router-dom';
+import formatCreatedAt from '../utils/formatCreatedAt';
 
 const Articles = () => {
     const [articles, setArticles] = useState([]);
@@ -17,17 +18,21 @@ const Articles = () => {
     return (
         <div>
             <Nav />
-            <h1>Articles</h1>
+            <h1 id='header'>Articles</h1>
             <ul>
               {articles.map((article) => {
                 console.log(article, '<<< article')
                 return (
                     <li className='article_list_item' key={article.article_id}> 
-                        <h3>{article.title}</h3>
-                        <h4>{article.author}</h4>
-                        <p>{article.comment_count}</p>      
-                        <p>{article.created_at}</p>
-                        <p>{article.votes}</p>              
+                        <h3 id='article_title'>{article.title}</h3>
+                        <h4 id='article_author'>{article.author}</h4>
+                        <p id='article_comment_count'>{article.comment_count} comments</p>      
+                        <p id='article_created_at'>
+                            <p id='article_date'>{formatCreatedAt(article.created_at)[0]}</p>
+                            <p id='article_time'>{formatCreatedAt(article.created_at)[1]}</p>
+                        </p>
+                        <p id='article_votes'>{article.votes}</p> 
+                        <button type='submit'><p>&#128077;</p></button>             
                     </li>
                 )
             })}
---
+++ src/utils/formatCreatedAt.js
@@ -0,0 +1,7 @@
+const formatCreatedAt = (created_at) => {
+    let [date, time] = created_at.split('T')
+    time = time.slice(0, 5);
+    return [date, time];
+}
+
+export default formatCreatedAt
