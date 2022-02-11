47454c09a4d5d1ae38b1544ef74ef62f7a722bea
rvallely <68432429+rvallely@users.noreply.github.com>
2022-02-11 13:41:29 +0000

    add error handling for Votes component

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -5,7 +5,9 @@ const Votes = ({article_id, votes}) => {
     const [voteChange, setVoteChange] = useState(0);
     const addVote = () => {
         setVoteChange((currChange) => currChange + 1);
-        patchArticle(article_id);
+        patchArticle(article_id).catch((err) => {
+            setVoteChange((currChange) => currChange - 1)
+        });
     }
     return <button onClick={() => addVote()}>{votes + voteChange}&#128077;</button>
 }
