5156d1bcdfb2a32c0469c99956e3a2598b318952
rvallely <68432429+rvallely@users.noreply.github.com>
2022-03-09 19:32:49 +0000

    style Header-link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 
 const Header = () => {
-    return <Link className='Header' key='home' to='/'>
+    return <Link className='Header-link' key='home' to='/'>
                <h1 className ='Header'>NC News</h1>
            </Link>;
 }
