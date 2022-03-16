9ad4751e8e6e4ba3e5918e4af52ca160b18f5284
rvallely <68432429+rvallely@users.noreply.github.com>
2022-03-16 17:24:06 +0000

    tidy up Articles code

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,52 +1,34 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { getArticles } from '../utils/api';
-import Nav from './Nav';
-import { useParams } from 'react-router-dom';
-import formatCreatedAt from '../utils/formatCreatedAt';
-import { HashLink as Link } from 'react-router-hash-link';
-import SortBy from './SortBy';
-import { useSearchParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useSearchParams, Link } from 'react-router-dom'
+import Date from './Date';
 import Error from './Error';
 import Header from './Header';
-import Date from './Date';
+import Nav from './Nav';
+import SortBy from './SortBy';
 import UserDisplay from './UserDisplay';
-
-
-
+import formatCreatedAt from '../utils/formatCreatedAt';
+import { getArticles } from '../utils/api';
 
 const Articles = () => {
-
     const [articles, setArticles] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    // const [topic, setTopic] = useState('');
     const [sortBy, setSortBy] = useState('');
     const [searchParams, setSearchParams] = useSearchParams();
-    
+    const [error, setError] = useState(null);
+
     const searchTopic = searchParams.get('topic')
     const searchSort_by = searchParams.get('sort_by')
     const searchOrder = searchParams.get('order')
 
-    const [error, setError] = useState(null);
-
-    // console.log('The sort by in state is ', sortBy)
-    // console.log('the topic in state is ', topic)
-    // let topic = '';
-    // if (searchTopic) {
-    //      topic=searchTopic;
-    // }
     let sort_by = '';
     if (searchSort_by && !searchOrder) {
         sort_by = `sort_by=${searchSort_by}`;
-   
     } 
     if (searchSort_by && searchOrder) {
-        sort_by = `sort_by=${searchSort_by} order=${searchOrder}`;
-        
+        sort_by = `sort_by=${searchSort_by} order=${searchOrder}`; 
     }
 
     useEffect(() => {
-
         getArticles(searchTopic, sortBy).then((articlesFromAPI) => {
             setArticles(articlesFromAPI);
             setIsLoading(false);
@@ -73,40 +55,38 @@ const Articles = () => {
                     <Error message={error.err.response.data.msg} status={error.err.response.status}/>
                 </div>
             )
-
         }
         else if (!error) {
             return (
-        <div>
-            <div className='header-date'>
-              <Header />  
-              <Date />
-         </div>
-        <UserDisplay /> 
-            <Nav />
-            <SortBy setSortBy={setSortBy}/>
+                <div>
+                    <div className='header-date'>
+                      <Header />  
+                      <Date />
+                    </div>
+                    <UserDisplay /> 
+                    <Nav />
+                    <SortBy setSortBy={setSortBy}/>
             
-            <ul className='article-list'>
-              {articles.map((article) => {
-                return (
-                    <li className='article-list-item' key={article.article_id}> 
-                        <Link className='link' key={article.article_id} to={`/articles/${article.article_id}`}>
-                            <h3 id='article-title'>{article.title}</h3>
-                        </Link>
-                        <h4 id='article-author'>{article.author}</h4>
-                        <Link className = 'link' to={`/articles/${article.article_id}#show-comments`}>{article.comment_count} comments</Link> 
-                        <div id='article-created_at'>
-                            <p id='article-date'>{formatCreatedAt(article.created_at)[0]}</p>
-                            <p id='article-time'>{formatCreatedAt(article.created_at)[1]}</p>
-                        </div>
-                        <p id='article-votes'>{article.votes} &#128077;</p> 
-                    </li>
-                );
-            })}
-            </ul>
-            
-        </div>
-    )
+                    <ul className='article-list'>
+                        {articles.map((article) => {
+                            return (
+                                <li className='article-list-item' key={article.article_id}> 
+                                    <Link className='link' key={article.article_id} to={`/articles/${article.article_id}`}>
+                                        <h3 id='article-title'>{article.title}</h3>
+                                    </Link>
+                                    <h4 id='article-author'>{article.author}</h4>
+                                    <Link className = 'link' to={`/articles/${article.article_id}#show-comments`}>{article.comment_count} comments</Link> 
+                                    <div id='article-created_at'>
+                                       <p id='article-date'>{formatCreatedAt(article.created_at)[0]}</p>
+                                       <p id='article-time'>{formatCreatedAt(article.created_at)[1]}</p>
+                                    </div>
+                                    <p id='article-votes'>{article.votes} &#128077;</p> 
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            )
         }   
     }
 }
