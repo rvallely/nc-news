ecbb64462bc823cb937f954f51a04e896bcbef30
rvallely <68432429+rvallely@users.noreply.github.com>
2022-03-16 00:32:49 +0000

    add alert to log in in order to vote

--- src/components/ArticleVotes.jsx
+++ src/components/ArticleVotes.jsx
@@ -1,8 +1,12 @@
 import { patchArticle } from '../utils/api';
 import { useState } from 'react';
+import { useContext } from 'react';
+import { UserContext } from '../contexts/User';
 
 const ArticleVotes = ({article_id, votes}) => {
     const [voteChange, setVoteChange] = useState(0);
+    const { loggedInUser, setLoggedInUser } = useContext(UserContext)
+
     const addVote = () => {
         setVoteChange((currChange) => currChange + 1);
         patchArticle(article_id).catch((err) => {
@@ -15,7 +19,12 @@ const ArticleVotes = ({article_id, votes}) => {
     //         setVoteChange((currChange) => currChange + 1)
     //     });
     // }
-    return <button onClick={() => addVote()}>{votes + voteChange}&#128077;</button>
+    if (loggedInUser.username === 'guest') {
+        return <button onClick={() => alert('Please go to User area and log in to vote! :)')}>{votes}&#128077;</button>
+    } else {
+        return <button onClick={() => addVote()}>{votes + voteChange}&#128077;</button>
+    }
+   
             {/* <button onClick={() => subtractVote()}>{votes - voteChange}&#128078;</button> */}
 
 }
--- src/components/CommentVotes.jsx
+++ src/components/CommentVotes.jsx
@@ -1,15 +1,23 @@
 import { patchComment } from '../utils/api';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { UserContext } from '../contexts/User';
+
 
 const CommentVotes = ({comment_id, votes}) => {
     const [voteChange, setVoteChange] = useState(0);
+    const { loggedInUser, setLoggedInUser } = useContext(UserContext)
+
     const addVote = () => {
         setVoteChange((currChange) => currChange + 1);
         patchComment(comment_id).catch((err) => {
             setVoteChange((currChange) => currChange - 1)
         });
     }
-    return <button onClick={() => addVote()}>{votes + voteChange}&#128077;</button>
+    if (loggedInUser.username === 'guest') {
+        return <button onClick={() => alert('Please go to User area and log in to vote! :)')}>{votes}&#128077;</button>
+    } else {
+        return <button onClick={() => addVote()}>{votes + voteChange}&#128077;</button>
+    }
 }
 
 export default CommentVotes;
