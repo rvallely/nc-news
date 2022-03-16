fd1e5f7c3cc8947802e02a6f49644a64a6554744
rvallely <68432429+rvallely@users.noreply.github.com>
2022-03-16 17:27:09 +0000

    tidy up ArticleVotes code

--- src/components/ArticleVotes.jsx
+++ src/components/ArticleVotes.jsx
@@ -1,9 +1,8 @@
-import { patchArticle } from '../utils/api';
-import { useState } from 'react';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '../contexts/User';
+import { patchArticle } from '../utils/api';
 
-const ArticleVotes = ({article_id, votes}) => {
+const ArticleVotes = ({ article_id, votes }) => {
     const [voteChange, setVoteChange] = useState(0);
     const { loggedInUser, setLoggedInUser } = useContext(UserContext)
 
@@ -13,20 +12,12 @@ const ArticleVotes = ({article_id, votes}) => {
             setVoteChange((currChange) => currChange - 1)
         });
     }
-    // const subtractVote = () => {
-    //     setVoteChange((currChange) => currChange - 1);
-    //     patchArticle(article_id, 'down').catch((err) => {
-    //         setVoteChange((currChange) => currChange + 1)
-    //     });
-    // }
+
     if (loggedInUser.username === 'guest') {
         return <button onClick={() => alert('Please go to User area and log in to vote! :)')}>{votes}&#128077;</button>
     } else {
         return <button onClick={() => addVote()}>{votes + voteChange}&#128077;</button>
     }
-   
-            {/* <button onClick={() => subtractVote()}>{votes - voteChange}&#128078;</button> */}
-
 }
 
 export default ArticleVotes;
