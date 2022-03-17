484e49e21f0a7ce41c469662128771a93d979ee8
rvallely <68432429+rvallely@users.noreply.github.com>
2022-03-17 15:23:40 +0000

    change sort by menu depending on which page coming from

--- src/components/Articles/SortBy.jsx
+++ src/components/Articles/SortBy.jsx
@@ -8,38 +8,67 @@ const SortBy = () => {
     
     let navigate = useNavigate();
     const location = useLocation()
-    console.log(location);
+
     let topic = undefined;
     if (searchTopic) {
         topic = searchTopic;
     }
 
-    return (
-        <div >            
-            <form className='dropdown'  >
-                <select 
-                  id='sort-by' 
-                  value={input}
-                  onChange={(e) => { 
-                    setInput(e.target.value);
-                        const formatQ = e.target.value.split(' ').join('&');
-                        if (topic === undefined) {
-                            navigate(`${location.pathname}?${formatQ}`);   
-                        } else {
-                            navigate(`${location.pathname}?topic=${topic}&${formatQ}`);   
-                        }
-                    }}> 
-                    <option disabled>Sort by: Most Recent</option>
-                    <option value='sort_by=created_at order=DESC'>Most Recent</option>
-                    <option value='sort_by=comment_count order=DESC'>Most Comments</option>
-                    <option value='sort_by=votes order=DESC'>Most Votes</option>
-                    <option value='sort_by=author order=ASC'>Author A-Z</option>
-                    <option value='sort_by=author order=DESC'>Author Z-A</option>
-                    <option value='sort_by=created_at order=ASC'>Least Recent</option>
-                </select>
-            </form>
-        </div>
-    )
+    if (location.pathname !== '/articles') {
+        return (
+            <div >            
+                <form className='dropdown'  >
+                    <select 
+                      id='sort-by' 
+                      value={input}
+                      onChange={(e) => { 
+                        setInput(e.target.value);
+                            const formatQ = e.target.value.split(' ').join('&');
+                            if (topic === undefined) {
+                                navigate(`${location.pathname}?${formatQ}`);   
+                            } else {
+                                navigate(`${location.pathname}?topic=${topic}&${formatQ}`);   
+                            }
+                        }}> 
+                        <option disabled>Sort by: Most Recent</option>
+                        <option value='sort_by=created_at order=DESC'>Most Recent</option>
+                        <option value='sort_by=comment_count order=DESC'>Most Comments</option>
+                        <option value='sort_by=votes order=DESC'>Most Votes</option>
+                        <option value='sort_by=created_at order=ASC'>Least Recent</option>
+                    </select>
+                </form>
+            </div>
+        )
+    } else {
+        return (
+            <div >            
+                <form className='dropdown'  >
+                    <select 
+                      id='sort-by' 
+                      value={input}
+                      onChange={(e) => { 
+                        setInput(e.target.value);
+                            const formatQ = e.target.value.split(' ').join('&');
+                            if (topic === undefined) {
+                                navigate(`${location.pathname}?${formatQ}`);   
+                            } else {
+                                navigate(`${location.pathname}?topic=${topic}&${formatQ}`);   
+                            }
+                        }}> 
+                        <option disabled>Sort by: Most Recent</option>
+                        <option value='sort_by=created_at order=DESC'>Most Recent</option>
+                        <option value='sort_by=comment_count order=DESC'>Most Comments</option>
+                        <option value='sort_by=votes order=DESC'>Most Votes</option>
+                        <option value='sort_by=author order=ASC'>Author A-Z</option>
+                        <option value='sort_by=author order=DESC'>Author Z-A</option>
+                        <option value='sort_by=created_at order=ASC'>Least Recent</option>
+                    </select>
+                </form>
+            </div>
+        )
+    }
+
+    
 }
 
 export default SortBy;
