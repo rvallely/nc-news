4f6aa5cbfffaaa76ec1ef8ad35ce6f4e7d373701
rvallely <68432429+rvallely@users.noreply.github.com>
2022-04-05 18:14:34 +0100

    add logo options

--- src/components/General/Header.jsx
+++ src/components/General/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
+import logo from '../../imgs/NC_transp.jpg';
 
 const Header = () => {
         return ( 
             <Link className='Header-link' key='articles' to='/articles'>
-                <h1 className ='Header'>NC News</h1>
+                {/* <h1 className ='Header'>NC News</h1> */}
+                <img className ='Header' src={logo} alt='nc logo'/>
             </Link>
         ) 
 }
