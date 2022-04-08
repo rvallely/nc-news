67f34f68143589fe3e7dfe8a4abd58fcb6014a29
rvallely <68432429+rvallely@users.noreply.github.com>
2022-04-08 13:01:31 +0100

    fix small typo on redirect msg

--- src/components/General/Redirect.jsx
+++ src/components/General/Redirect.jsx
@@ -18,9 +18,9 @@ const Redirect = () => {
                 <h3>Whoops, looks like that page doesn't exist &#128579;</h3>
                 <p>We are sorry we were unable to bring you the page that you requested. <br></br>
                 Please:<br></br>
-                - check the url you entered is correct.<br></br>
-                - hit the refresh button in your browser.<br></br>
-                - use the NC News Navigation Bar.</p>
+                - check the url you entered is correct<br></br>
+                - hit the refresh button in your browser<br></br>
+                - use the NC News Navigation Bar</p>
             </div>
         </div>
 
